8a7c8f3d1c0719d9364adc88144de5513948dab0
Swayve <emmannuelalade2008@gmail.com>
2023-05-12 12:25:19 +0200

    initial commit

---
+++ YoutubePollWeb/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Thumbnail
+
+def compare_thumbnails(request):
+    # Retrieve two random thumbnails from the database
+    thumbnails = Thumbnail.objects.order_by('?')[:2]
+
+    # Render the compare.html template with the thumbnails as context
+    context = {'thumbnails': thumbnails}
+    return render(request, 'compare.html', context)
